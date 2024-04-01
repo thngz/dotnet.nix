827cddaedd9e1d342081b05aa139cd647bce5d4b
gkiviv <georgkivivali7@gmail.com>
2024-04-01 16:38:49 +0300

    added db support

--- flake.nix
+++ flake.nix
@@ -16,13 +16,35 @@
             jetbrains.rider 
             dotnetCorePackages.dotnet_8.sdk
             postgresql
+            bruno
         ];
 
         shellHook = ''
-          echo "i run"
-          dotnet new tool-manifest
-          dotnet tool install dotnet-ef
-          dotnet tool install dotnet-aspnet-codegenerator
+            echo "i run"
+            dotnet new tool-manifest
+            dotnet tool install dotnet-ef
+            dotnet tool install dotnet-aspnet-codegenerator
+            
+            #Start postgres
+            
+            export PG=$PWD/.dev_postgres/
+            export PGDATA=$PG/data
+            export PGPORT=5432
+            export PGHOST=localhost
+            export PGUSER=$USER
+            export PGPASSWORD=postgres
+            export PGDATABASE=piletiring
+            export DB_URL=postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE
+
+            pg_setup() {
+              pg_ctl -D $PGDATA stop;
+              rm -rf $PG;
+              initdb -D $PGDATA &&
+              echo "unix_socket_directories = '$PGDATA'" >> $PGDATA/postgresql.conf &&
+              pg_ctl -D $PGDATA -l $PG/postgres.log start &&
+              createdb
+            }
+            pg_setup
         '';
       };
     };
